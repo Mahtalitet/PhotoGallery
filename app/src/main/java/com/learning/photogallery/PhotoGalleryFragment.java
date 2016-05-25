1cc547e9e991852e3612794f01c67a867e3cdc82
ilya <MAAFI4I@gmail.com>
2016-05-25 11:56:49 +0200

    Changed the minSdkVersion from 8 to 11

--- app/src/main/java/com/learning/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/learning/photogallery/PhotoGalleryFragment.java
@@ -23,6 +23,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.learning.photogallery.gallery.GalleryItem;
 
