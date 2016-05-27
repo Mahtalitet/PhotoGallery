5ca6e8ceb57d49edfda7200b3b2b8534d56e0675
ilya <MAAFI4I@gmail.com>
2016-05-27 18:35:10 +0200

    Added notifications with NotificationCompat

--- app/src/main/java/com/learning/photogallery/PollService.java
+++ app/src/main/java/com/learning/photogallery/PollService.java
@@ -2,11 +2,14 @@ package com.learning.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,7 +21,7 @@ import java.util.ArrayList;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
-    private static final int POLL_INTERVAL = 1000 * 15;
+    private static final int POLL_INTERVAL = 1000 * 60 * 5;
 
     public PollService() {
         super(TAG);
@@ -49,6 +52,22 @@ public class PollService extends IntentService {
 
         if (!resultId.equals(lastResultId)) {
             Log.i(TAG, "Get a new result: "+resultId);
+
+            PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, PhotoGalleryActivity.class), 0);
+
+            NotificationCompat.Builder notificationCompatBuilder = new NotificationCompat.Builder(this)
+                    .setContentTitle(getResources().getString(R.string.new_pictures_title))
+                    .setContentText(getResources().getString(R.string.new_pictures_text))
+                    .setSmallIcon(R.drawable.ic_photo)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_SOUND)
+                    .setContentIntent(pi);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, notificationCompatBuilder.build());
+
+
         } else {
             Log.i(TAG, "Get an old result: "+resultId);
         }
